f2afef064c106e9769016ab7f91143e4e735d263
4strodev <jumarurex@gmail.com>
2023-10-23 09:42:12 +0200

    initial commit

---
+++ lib.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type environmentField struct {
+	name     string
+	required bool
+}
+
+// Loads environment variables to struct fields
+// struct fields cannot be complex data: slices, maps, structs, pointers, etc.
+func Marshal(v any) (error) {
+	val := reflect.ValueOf(v)
+
+	if val.Kind() != reflect.Pointer {
+		return errors.New("struct pointer expected")
+	}
+
+	structVal := val.Elem()
+	if structVal.Kind() != reflect.Struct {
+		return errors.New("struct pointer expected")
+	}
+
+	structType := structVal.Type()
+	var envField environmentField = environmentField{name: "", required: true}
+	for i := 0; i < structVal.NumField(); i++ {
+		vfield := structVal.Field(i)
+		tfield := structType.Field(i)
+
+		// Cechking if field has env tag
+		tagString, exists := tfield.Tag.Lookup("env")
+		if !exists {
+			envField.name = tfield.Name
+		} else {
+			var err error
+			envField, err = tagToEnvField(tagString)
+			if err != nil {
+				return err
+			}
+
+			if envField.name == "" {
+				envField.name = tfield.Name
+			}
+		}
+
+		// Reading env variable
+		envValue := os.Getenv(envField.name)
+		if envValue == "" {
+			if envField.required {
+				return fmt.Errorf("'%s' is not defined and is required", envField.name)
+			}
+			continue
+		}
+		// Parse string to corresponding type
+		switch tfield.Type.Kind() {
+		case reflect.Int:
+			parsedVal, err := strconv.ParseInt(envValue, 10, 32)
+			if err != nil {
+				return err
+			}
+			vfield.SetInt(parsedVal)
+		case reflect.Int8:
+			parsedVal, err := strconv.ParseInt(envValue, 10, 8)
+			if err != nil {
+				return err
+			}
+			vfield.SetInt(parsedVal)
+		case reflect.Int16:
+			parsedVal, err := strconv.ParseInt(envValue, 10, 16)
+			if err != nil {
+				return err
+			}
+			vfield.SetInt(parsedVal)
+		case reflect.Int32:
+			parsedVal, err := strconv.ParseInt(envValue, 10, 32)
+			if err != nil {
+				return err
+			}
+			vfield.SetInt(parsedVal)
+		case reflect.Int64:
+			parsedVal, err := strconv.ParseInt(envValue, 10, 64)
+			if err != nil {
+				return err
+			}
+			vfield.SetInt(parsedVal)
+		case reflect.Uint:
+			parsedVal, err := strconv.ParseUint(envValue, 10, 32)
+			if err != nil {
+				return err
+			}
+			vfield.SetUint(parsedVal)
+		case reflect.Uint8:
+			parsedVal, err := strconv.ParseUint(envValue, 10, 8)
+			if err != nil {
+				return err
+			}
+			vfield.SetUint(parsedVal)
+		case reflect.Uint16:
+			parsedVal, err := strconv.ParseUint(envValue, 10, 16)
+			if err != nil {
+				return err
+			}
+			vfield.SetUint(parsedVal)
+		case reflect.Uint32:
+			parsedVal, err := strconv.ParseUint(envValue, 10, 32)
+			if err != nil {
+				return err
+			}
+			vfield.SetUint(parsedVal)
+		case reflect.Uint64:
+			parsedVal, err := strconv.ParseUint(envValue, 10, 64)
+			if err != nil {
+				return err
+			}
+			vfield.SetUint(parsedVal)
+		case reflect.Bool:
+			parsedVal, err := strconv.ParseBool(envValue)
+			if err != nil {
+				return err
+			}
+			vfield.SetBool(parsedVal)
+		case reflect.Float32:
+			parsedVal, err := strconv.ParseFloat(envValue, 32)
+			if err != nil {
+				return err
+			}
+			vfield.SetFloat(parsedVal)
+		case reflect.Float64:
+			parsedVal, err := strconv.ParseFloat(envValue, 64)
+			if err != nil {
+				return err
+			}
+			vfield.SetFloat(parsedVal)
+		case reflect.String:
+			vfield.SetString(envValue)
+		default:
+			return fmt.Errorf("field '%s' of type %s' is not admited", tfield.Name, tfield.Type.Kind())
+		}
+	}
+
+	return nil
+}
+
+// Parse `env` tag
+func tagToEnvField(tag string) (environmentField, error) {
+	envField := environmentField{}
+	splitTag := strings.Split(tag, ",")
+
+	if len(splitTag) == 0 {
+		return envField, errors.New("empty tag not allowed")
+	}
+
+	for i, v := range splitTag {
+		splitTag[i] = strings.Trim(v, "\\s\\n\\t")
+	}
+
+	if len(splitTag) == 2 {
+		envField.name = splitTag[0]
+		if splitTag[1] != "required" {
+			return envField, fmt.Errorf("env tag not valid expected 'required' found '%s'", splitTag[1])
+		}
+		envField.required = true
+	} else if len(splitTag) == 1 {
+		if splitTag[0] == "required" {
+			envField.required = true
+		} else {
+			envField.name = splitTag[0]
+		}
+	} else {
+		return envField, fmt.Errorf("invalid env tag")
+	}
+
+	return envField, nil
+}
---
+++ lib_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestCase[T any] struct {
+	value   T
+	success bool
+}
+
+type EnvVariables struct {
+	Workers       uint8   `env:"WORKERS,required"`
+	ApiToken      string  `env:",required"`
+	RemoteLogging bool    `env:"REMOTE_LOGGING"`
+	Delay         float32 `env:"DELAY,required"`
+}
+
+func TestTagToEnvField(t *testing.T) {
+	testCases := []TestCase[string]{
+		{
+			value:   "WORKER",
+			success: true,
+		},
+		{
+			value:   "WORKER,requried",
+			success: false,
+		},
+		{
+			value:   "required",
+			success: true,
+		},
+		{
+			value:   ",required",
+			success: true,
+		},
+	}
+
+	for i, testCase := range testCases {
+		_, err := tagToEnvField(testCase.value)
+		var message string
+		if testCase.success {
+			message = fmt.Sprintf("test case '%d':'%s' failed: '%s'", i+1, testCase.value, err)
+		} else {
+			message = fmt.Sprintf("test case '%d':'%s' should fail", i+1, testCase.value)
+		}
+		assert.Equal(t, err == nil, testCase.success, message)
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	godotenv.Load(".env.test")
+
+	expectedResult := EnvVariables{
+		Workers: 10,
+		ApiToken: "some api token",
+		RemoteLogging: false,
+		Delay: 3.5,
+	}
+
+	var envVariables EnvVariables
+	err := Marshal(&envVariables)
+	assert.Empty(t, err, err)
+	assert.Equal(t, expectedResult, envVariables)
+}
